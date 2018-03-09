9e7318fb17fa6ff44437677a612ea6b9595e7d8e
Chris Auld <chris.auld@auld.nz>
2018-03-09 01:41:49 -0800

    More packages & Fixed LightGBM Install

--- install-r-packages.r
+++ install-r-packages.r
@@ -2,13 +2,14 @@
 # But, makes it easier to scan and see what you're getting and to add comments to each line for wierd stuff.
 
 #Wrapper for readability
-i.p <- install.packages #TODO: Flesh this out a bit so it builds and emits a list of packages installed with Google search URLs
+i.p <- require #TODO: Flesh this out a bit so it builds and emits a list of packages installed with Google search URLs
 
 
 #Useful for installing stuff that isn't in CRAN and building stuff
 i.p("devtools")
-i.p("roxygen2")i.p("quantmod")
 i.p("testthat")
+i.p("curl")
+i.p("httr")
 
 #Tidyverse
 i.p("tidyverse")
@@ -28,22 +29,16 @@ i.p("knitr")
 
 #Quant
 i.p("quantmod")
-i.p("quantstrat")
 i.p("PortfolioAnalytics")
 
 #ML
+set_config(config(ssl_verifypeer = 0L))
 i.p("xgboost") # http://xgboost.readthedocs.io/en/latest/build.html#installing-r-package-with-gpu-support
-install_github("Microsoft/LightGBM", subdir = "R-package") # GPU support here: https://github.com/Microsoft/LightGBM/tree/master/R-package 
-
-#Graphics
-
-
-#install.packages(c("remotes","readxl","googlesheets","haven", "readr", "rio", "Hmisc", "sqldf", 
-#                   "jsonlite", "XML", "httr", "quantmod", "tidyquant", 
-##                   "rvest", "dplyr", "purrr", "reshape2", "tidyr", 
-#                   "magrittr", "validate", "testthat", "data.table", "stringr", "lubridate", "zoo", "editR", "knitr", "officer", 
-#                   "listviewer", "DT", "ggplot2", "ggiraph", "dygraphs", "googleVis", "metricsgraphics", "RColorBrewer", 
-#                   "sf", "leaflet", "ggmap", "tmap", "tmaptools", "mapsapi", "tidycensus", "glue", "rga", 
-#                   "RSiteCatalyst", "roxygen2", "shiny", "flexdashboard", "openxlsx", "gmodels", "janitor", "car", 
-#                   "rcdimple", "foreach", "scales", "plotly", "highcharter", "profvis", "tidytext", "diffobj", "Prophet", 
-#                   "feather", "fst", "googleAuthR", "cloudyR"))
+devtools::install_github("Laurae2/lgbdl")
+library(lgbdl)
+lgb.dl.fixed(commit = "master",
+       compiler = "gcc",
+       repo = "https://github.com/Microsoft/LightGBM",
+       cores = 4,
+       use_gpu = TRUE)
+devtools::install_github('catboost/catboost', subdir = 'catboost/R-package')
